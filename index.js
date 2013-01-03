a8b6e6c27101d63390fae49e1034ce95ba247f15
Seth McLaughlin <s@sethmcl.com>
2013-01-03 08:19:22 -0800

    Fixes issue #1 - do not initialize until Phantom() is called.

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var PhantomJsPlease = require( './lib/PhantomJsPlease' );
 
 // 'Static' instance
 var instance = new PhantomJsPlease();
-instance.initialize();
 
 module.exports = function() {
+
+  if( !instance.initialized ){
+    instance.initialize();
+  }
+
   return instance.createBrowser();
 };
